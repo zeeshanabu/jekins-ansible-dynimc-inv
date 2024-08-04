4456ab51641e059968582f8d0d256b5fba508a7d
zeeshanabu <167482818+zeeshanabu@users.noreply.github.com>
2024-08-04 15:42:05 -0400

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region="us-east-1" 
 }
 
-resource "aws_Instance" "AWSEC2Instance"{
+resource "aws_instance" "AWSEC2Instance"{
 ami = "ami-0ba9883b710b05ac6"
 instance_type = "t2.micro"
 key_name = "ansible"
